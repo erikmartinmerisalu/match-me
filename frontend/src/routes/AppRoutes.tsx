c79954ef304c74b97110f159e9a2365696b77bcc
Andero <andero.sc@gmail.com>
2025-09-30 15:38:52 +0300

    Added reccomendaton page, a basic plan with component render, changed routing.

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -4,6 +4,8 @@ import SignUp from '../auth/SignUp'
 import {  useAuth } from '../context/AuthContext'
 import UserProfile from '../pages/userprofile/UserProfile';
 import NotFound from '../pages/notfound/NotFound';
+import Match from '../pages/matches/Match';
+import Home from '../pages/frontpage/Home';
 
 export default function AppRoutes() {
   const {loggedIn} = useAuth();
@@ -11,14 +13,15 @@ export default function AppRoutes() {
   return (
     <div>
         <Routes>
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/*" element={<NotFound />} />
 
-
-
           {loggedIn === true ? <>
             <Route path="/userprofile" element={<UserProfile />} />
+            <Route path="/match" element={<Match />} />
+
           </> :
           <Route path="/login" element={<LogIn />} />
           }
