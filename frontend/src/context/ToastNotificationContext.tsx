298afdc066c30855e58639155df0e5fafcad3947
Andero <andero.sc@gmail.com>
2025-10-15 15:26:04 +0300

    Added toast setup fro frontend
    Former-commit-id: aefa5d3831f5b486f98a61eddce5f784949191f9 [formerly b98d2a69fb9bfddb36c67cd1938c26068c1c039d] [formerly 3d1decbf04b12b0491981d4e3f6a9ab7d682f8ec [formerly c9d265295789b3d1f762f0068ce7ad8f90fd4590]]
    Former-commit-id: 3d1decbf04b12b0491981d4e3f6a9ab7d682f8ec
    Former-commit-id: f1c5de724f39a75977e69c2b535ef4e5738fbcb8

---
+++ frontend/src/context/ToastNotificationContext.tsx
@@ -0,0 +1,97 @@
+import React, {
+  createContext,
+  useState,
+  ReactNode,
+  useContext,
+  useEffect,
+} from "react";
+
+export enum NotificationTypes {
+  success = "success",
+  error = "error",
+  info = "info",
+}
+
+// notification object
+export interface Notification {
+  id: string;
+  message: string;
+  type: NotificationTypes;
+  duration?: number; // milliseconds to show
+}
+
+interface NotificationContextProps {
+  notificationsList: Notification[];
+  addNotification: (notification: Notification) => void;
+  removeNotification: (id: string) => void;
+  CustomComponent?: React.ElementType;
+}
+
+const NotificationContext = createContext<NotificationContextProps | undefined>(
+  undefined
+);
+
+export const useNotification = (): NotificationContextProps => {
+  const context = useContext(NotificationContext);
+  if (!context) {
+    throw new Error(
+      "useNotification must be used within a NotificationProvider"
+    );
+  }
+  return context;
+};
+
+// This file name: ToastNotificationContext
+
+interface ToastNotificationProviderProps {
+  children: ReactNode;
+  customComponent?: React.ElementType;
+}
+
+export const ToastNotificationProvider = ({
+  children,
+  customComponent: CustomComponent,
+}: ToastNotificationProviderProps) => {
+  const [notificationsList, setNotificationsList] = useState<Notification[]>(
+    []
+  );
+
+  const addNotification = (notification: Notification) => {
+    setNotificationsList((prevNotifications) => [
+      ...prevNotifications,
+      notification,
+    ]);
+  };
+
+  const removeNotification = (id: string) => {
+    setNotificationsList((prevNotifications) =>
+      prevNotifications.filter((notification) => notification.id !== id)
+    );
+  };
+
+  // remove notifications after a timeout
+  useEffect(() => {
+    notificationsList.forEach((notification) => {
+      if (notification.duration) {
+        const timeoutId = setTimeout(() => {
+          removeNotification(notification.id);
+        }, notification.duration);
+
+        return () => clearTimeout(timeoutId);
+      }
+    });
+  }, [notificationsList]);
+
+  return (
+    <NotificationContext.Provider
+      value={{
+        notificationsList,
+        addNotification,
+        removeNotification,
+        CustomComponent,
+      }}
+    >
+      {children}
+    </NotificationContext.Provider>
+  );
+};
