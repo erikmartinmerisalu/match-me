a090d28207487796674c0b8c33992fa11e151891
Andero <andero.sc@gmail.com>
2025-11-23 18:24:29 +0200

    Changes before patch

--- backend/src/main/java/com/matchme/service/RecommendationService.java
+++ backend/src/main/java/com/matchme/service/RecommendationService.java
@@ -11,7 +11,6 @@ import com.matchme.entity.UserProfile;
 import com.matchme.repository.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.Period;
